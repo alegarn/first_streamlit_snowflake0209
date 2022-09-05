68673c06cae23adc65d44e82d906701eb4d40a87
alegarn <97517147+alegarn@users.noreply.github.com>
2022-09-06 01:15:24 +0200

    🥋 Let's Query Some Data, Instead

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,13 +2,6 @@ import snowflake.connector
 import pandas
 import streamlit
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-my_data_row = my_cur.fetchone()
-streamlit.text("Hello from Snowflake:")
-streamlit.text(my_data_row)
-
 streamlit.title("My parent's new healthy dinner!")
 
 streamlit.header(' Breakfast Menu')
@@ -41,3 +34,11 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_c
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
 streamlit.dataframe(fruityvice_normalized)
+
+
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT * from fruit_load_list")
+my_data_row = my_cur.fetchone()
+streamlit.text("The fruit load contains:")
+streamlit.text(my_data_row)
