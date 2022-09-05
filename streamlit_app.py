5da537e79504adaeb97d4af7f2695558fac59235
alegarn <97517147+alegarn@users.noreply.github.com>
2022-09-06 01:11:23 +0200

    Let's Query Our Trial Account Metadata

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,13 @@ import snowflake.connector
 import pandas
 import streamlit
 
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
+
 streamlit.title("My parent's new healthy dinner!")
 
 streamlit.header(' Breakfast Menu')
