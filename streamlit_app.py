1cc30f39c60c4d0dde7c39281e00e0e7dc71d9dd
alegarn <97517147+alegarn@users.noreply.github.com>
2022-09-06 03:05:10 +0200

    Write Code to Add Rows to Our Fruit List in Snowflake

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import snowflake.connector
 import pandas as pd
 import streamlit
+import requests
+from urllib.error import URLError
 
 streamlit.title("My parent's new healthy dinner!")
 
@@ -27,7 +29,6 @@ streamlit.header("Fruityvice Fruit Advice!")
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
 
-import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
 # write your own comment -what does the next line do?
@@ -35,7 +36,7 @@ fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
 streamlit.dataframe(fruityvice_normalized)
 
-
+streamlit.stop()
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT * from fruit_load_list")
