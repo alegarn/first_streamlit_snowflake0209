829f08e36ca68107f666714bf2dc5d02da1673d3
alegarn <97517147+alegarn@users.noreply.github.com>
2022-09-06 02:49:43 +0200

    add_my_fruit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import snowflake.connector
-import pandas
+import pandas as pd
 import streamlit
 
 streamlit.title("My parent's new healthy dinner!")
@@ -12,10 +12,10 @@ streamlit.text("🥑🍞 Avocado Toast")
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
-# Let's put a pick list here so they can pick the fruit they want to include 
+# Let's put a pick list here so they can pick the fruit they want to include
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
@@ -30,8 +30,8 @@ streamlit.write('The user entered ', fruit_choice)
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
-# write your own comment -what does the next line do? 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# write your own comment -what does the next line do?
+fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
 streamlit.dataframe(fruityvice_normalized)
 
@@ -43,10 +43,9 @@ my_data_rows = my_cur.fetchall()
 print(type(my_data_rows))
 streamlit.header("The fruit load contains:")
 streamlit.dataframe(my_data_rows)
+print(my_data_rows)
 
 
-
-# Let's put a pick list here so they can pick the fruit they want to include 
-add_my_fruit = streamlit.multiselect("Pick some fruits:", list(my_data_rows.index), ['Banana'])
-fruits_to_show = my_data_rows.loc[add_my_fruit]
-streamlit.text("Thanks for adding" + add_my_fruit)
+# Let's put a pick list here so they can pick the fruit they want to include
+add_my_fruit = streamlit.multiselect("Pick some fruits:", my_data_rows, [('banana',)])
+streamlit.text("Thanks for adding " + add_my_fruit[0][0])
